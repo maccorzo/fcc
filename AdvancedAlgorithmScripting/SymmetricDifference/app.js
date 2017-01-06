9b26334adf1fcd31c20e5d071b7fa30b59ea9572
maccorzo <marko@hankkila.me>
2017-01-06 19:35:44 +0200

    ready

--- AdvancedAlgorithmScripting/SymmetricDifference/app.js
+++ AdvancedAlgorithmScripting/SymmetricDifference/app.js
@@ -1,28 +1,26 @@
 function sym(args) {
+  // convert arguments object to array
   var arr = Array.prototype.slice.call(arguments);
-
   rArr = [];
 
   for (var i = 0 ; i < arr.length ; i++) {
-
     rArr = arr[i]
+    // remove duplicate numbers
     .filter(function(elem, index, self) {
       return index == self.indexOf(elem);
     })
+    // compare arrays
     .filter(function(el) {
       return rArr.indexOf(el) == -1;
     })
     .concat(rArr.filter(function(el) {
-      return arr[i].indexOf(el) == -1
+      return arr[i].indexOf(el) == -1;
     }))
     .sort();
-    
   }
   return rArr;
 }
 
-
+// test
 console.log(sym([1, 2, 3], [5, 2, 1, 4]));
-//
-//
 console.log(sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1]));
