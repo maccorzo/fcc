200d9054b34218040a96617f9951fb156109a820
maccorzo <marko@hankkila.me>
2017-01-16 20:54:57 +0200

    refactor generator function

--- twitchtv/js/app.js
+++ twitchtv/js/app.js
@@ -12,34 +12,21 @@ function* steps() {
   const streamers = ['EULCS1','ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas']
   const userJson = [];
   const streamJson = [];
-  userJson.push(yield ajax(apiUrlUsers+streamers[0]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[1]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[2]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[3]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[4]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[5]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[6]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[7]));
-  userJson.push(yield ajax(apiUrlUsers+streamers[8]));
-
-  streamJson.push(yield ajax(apiUrlStreams+streamers[0]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[1]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[2]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[3]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[4]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[5]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[6]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[7]));
-  streamJson.push(yield ajax(apiUrlStreams+streamers[8]));
+  let index = 0;
+  // push users and streamers in array
+  while(index < streamers.length) {
+    userJson.push(yield ajax(apiUrlUsers+streamers[index]));
+    streamJson.push(yield ajax(apiUrlStreams+streamers[index]));
+    index++;
+  }
   yield parseData(userJson, streamJson);
+
 }
 
-function parseData(user, stream) {
-  console.log(user, stream);
-  for(line of user) {
-    console.log(line.stream);
-  }
+function parseData(users, streams) {
+  console.log(users, streams);
 }
 
 const gen = steps();
 gen.next();
+
