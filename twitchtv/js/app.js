13e004442468ce20ea0d7613a4bea2eb77e74413
maccorzo <marko@hankkila.me>
2017-01-21 17:55:37 +0200

    refactor fetchig

--- twitchtv/js/app.js
+++ twitchtv/js/app.js
@@ -1,6 +1,7 @@
 /*eslint-disable */
 const apiUrlUsers = 'https://wind-bow.gomix.me/twitch-api/users/';
 const apiUrlStreams = 'https://wind-bow.gomix.me/twitch-api/streams/';
+const jsonData = [];
 
 function ajax(url) {
   fetch(url)
@@ -10,33 +11,34 @@ function ajax(url) {
 
 function* steps() {
   const streamers = ['EULCS1','ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas']
-  const userJson = [];
-  const streamJson = [];
   let index = 0;
   // push users and streamers in array
   while(index < streamers.length) {
-    userJson.push(yield ajax(apiUrlUsers+streamers[index]));
-    streamJson.push(yield ajax(apiUrlStreams+streamers[index]));
-    index++;
+    let temp;
+    jsonData[index] = yield ajax(apiUrlUsers+streamers[index]);
+    temp = yield ajax(apiUrlStreams+streamers[index]);
+    jsonData[index].stream = temp.stream;
+    jsonData[index].online = jsonData[index].stream !== null;
+    index++;  
   }
-  yield parseData(userJson, streamJson);
+  yield showData();  
 }
 
-function parseData(users, streams) {
-  console.log(users, streams);
+function showData(filter) {
+  console.log(jsonData);
   let template = '';
   const el = document.querySelector('#app')
+  const filteredData = filter === undefined ? jsonData : jsonData.filter(el => el.online === filter);
 
-  for (let x = 0; x < users.length; x += 1) {
-    const {display_name, logo, name, bio} = users[x];
-    const online = streams[x].stream !== null;
+  for (let x = 0; x < filteredData.length; x += 1) {
+    const {display_name, logo, name, bio, online} = filteredData[x];
     const temp = `
     <div class="box ${online === true ? 'online' : 'offline'}">
-      <img class="img img-responsive" src="${logo}"> 
-      <div class="info-box">
-        <h3>${display_name}</h3>
-        <p>${bio !== null ? bio : ''}</p>
-      </div>
+    <img class="img img-responsive" src="${logo}"> 
+    <div class="info-box">
+    <h3>${display_name}</h3>
+    <p>${bio !== null ? bio : ''}</p>
+    </div>
     </div>
     `;
     template += temp;
