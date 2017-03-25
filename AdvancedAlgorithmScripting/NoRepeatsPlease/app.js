b1efeff8b289f412ce1dc99d8722948111f2ff14
maccorzo <marko@hankkila.me>
2017-03-25 13:00:16 +0200

    add permute function

--- AdvancedAlgorithmScripting/NoRepeatsPlease/app.js
+++ AdvancedAlgorithmScripting/NoRepeatsPlease/app.js
@@ -1,5 +1,25 @@
 function permAlone(str) {
-  return str;
+  var results = [];
+  
+  function permute(arr, prev) {
+    var cur;
+    prev = prev || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0 && prev.concat(cur).join('').match(/([a-zA-Z])\1/) === null) {
+        results.push(prev.concat(cur));
+      }
+      permute(arr.slice(), prev.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+  
+  return permute(str.split('')).length;
+  
 }
 
-permAlone('aab');
+const answer = permAlone('aab');
+console.log(answer);
